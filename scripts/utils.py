4ada206d5df7ad921bfb1728632855d04695a70b
Felippe Andrade <4drade@gmail.com>
2024-07-12 12:53:42 -0300

    docs(utils): improve code documentation and readability
    Enhances the Utils class with detailed docstrings for better understanding of methods' purposes and usage. Refactors the mount_elements method to use a more Pythonic dictionary comprehension, improving code clarity and maintainability.

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,14 +2,34 @@ from pathlib import Path
 
 
 class Utils:
+    """
+    This class contains some utility methods.
+    """
 
     @staticmethod
     def read_file(file: Path) -> list[str]:
+        """
+        Reads the content of a file and returns it as a list of strings.
+
+        Args:
+            file: The file to be read.
+
+        Returns:
+            A list of strings, one for each line in the file.
+        """
         with open(file, 'r') as f:
             content = f.readlines()
-        # Remover o '\n' do final de cada linha
         return [line.strip() for line in content]
 
     @staticmethod
     def mount_elements(column_names: list[str]) -> dict:
-        return dict((column, column) for column in column_names)
+        """
+        Mounts a dictionary of elements from a list of column names.
+
+        Args:
+            column_names: The names of the columns to generate the elements from.
+
+        Returns:
+            A dictionary with the column names as keys and the same column names as values.
+        """
+        return {column: column for column in column_names}
